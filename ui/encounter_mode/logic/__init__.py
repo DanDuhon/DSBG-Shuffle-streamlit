6c769e32adca698db7d51d182bdf39f33f63ab79
DanDuhon <49920618+DanDuhon@users.noreply.github.com>
2026-01-24 09:32:48 -0600

    Added missing import.

--- ui/encounter_mode/logic/__init__.py
+++ ui/encounter_mode/logic/__init__.py
@@ -16,6 +16,10 @@ from ui.encounter_mode.generation import (
 from core.enemies import ENEMY_EXPANSIONS_BY_ID
 from ui.encounter_mode.data.enemies import enemyNames
 from ui.encounter_mode.data.rewards import ENCOUNTER_ORIGINAL_REWARDS
+from ui.encounter_mode.data.behavior_modifiers import (
+    ENCOUNTER_BEHAVIOR_MODIFIERS,
+    ENCOUNTER_BEHAVIOR_MODIFIERS_EDITED,
+)
 from core.character.character_stats import average_souls_to_equip
 from ui.character_mode.data_io import _find_data_file, _load_json_list
 
